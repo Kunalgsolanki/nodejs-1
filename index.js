d312fa8775c169ae4d9139f196498bf7efd21b70
Kunalgsolanki <kgs2002107@gmail.com>
2022-10-01 18:38:02 +0530

    day  2

--- index.js
+++
@@ -1,5 +0,0 @@
-const app  = require ( './app');
-const apk = require ( './apk');
-
-console.log( app.z());
-console.log( apk.o);
---
+++ node js/index.js
@@ -0,0 +1,47 @@
+const app  = require ( './app');
+const apk = require ( './apk');
+
+console.log( app.z());
+console.log( apk.o);
+
+const arr = [ 1,2,3,5,2,1];
+ arr.filter((item)=>{
+    
+   console.log( item)
+ })
+
+
+
+ var kgs = [ " kunal", " mithil", " param ", " aniket"]
+
+
+
+  kgs.filter (  (item)=>{
+   
+   console.log(  item)
+
+ })
+
+   let result =  fy1 = [ 1,2,3,,6,7,8,9,10,11,12,13,14,15,16,17,18]
+
+
+
+
+   var  ksg = fy1. filter ((  item)=> {
+
+    return  item < 9 
+
+ })
+
+ console.log( ksg );
+
+
+
+  
+
+
+
+
+
+
+
