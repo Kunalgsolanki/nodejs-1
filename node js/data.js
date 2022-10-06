d45146ffc5fc847ba6976ce39b92db6c9040283c
Kunalgsolanki <kgs2002107@gmail.com>
2022-10-06 22:56:30 +0530

    kunal

---
+++ node js/data.js
@@ -0,0 +1,22 @@
+
+const data = [
+    
+    {
+    name: 'Kunal g solanki',
+    email: 'kgs2002107@gmail.com',
+    skills: 'web devloper '
+},
+
+{
+    name: ' Mithil Maske',
+    email: 'MithilMaske@gmailcom',
+    skills: ' Ai devloper '
+},
+{
+    name: ' Param N Pathak',
+    email: ' Param2092929@gmail.com',
+    skills: ' QuntomReasercher '
+},
+
+]
+module.exports = data;
--- node js/index.js
+++ node js/index.js
@@ -1,2 +1,26 @@
+///  How to make  make simple api tutorial
 
-console.log("hey i am kunal g solanki")
+// first make server 
+
+const http = require('http');
+const { json } = require('stream/consumers');
+const data = require('./data');
+
+  http.createServer (
+
+     (req, resp)=>{
+         
+
+         resp.writeHead(200,
+             {'Content-Type': 'application\json'}
+            );
+
+         resp.write(JSON.stringify(data))
+
+         resp.end();
+     }
+  ).listen(2000)
+
+
+
+ 
